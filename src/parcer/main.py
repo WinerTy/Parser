76c813b4fd7814fdaa008c36776b501de72d9d62
WinerTy <nikitokmay@gmail.com>
2025-05-19 15:11:48 +0300

    Feat: Base categorize product sustym

--- src/parcer/main.py
+++ src/parcer/main.py
@@ -3,15 +3,36 @@ import pandas as pd
 
 import numpy as np
 from typing import List
+import re
 
 file_path = "data/data.xlsx"
+
+
+category_rule = {
+    "Двигатель": {
+        "Амортизаторы": ["амортизатор", "подвеска"],
+        "Распредвал": ["втулка распределительного вала"],
+        "Опоры": ["подушка двигателя"],
+        "Система Зажигания": ["зажигания", "свеча"],
+    },
+    "Тормозная Система": {"Барабаны": ["барабан"], "Втулки": ["втулка"]},
+    "Ходовая часть": {"Стабилизаторы": ["втулка-подушка", "подушка"]},
+    "Крепеж": {"Гайки": ["гайки"]},
+}
 
 
 class ExselParser:
     def __init__(self, file_path: str):
         self.file = load_workbook(file_path)
         self.sheet = self.file.active
-        self.start_row = self.find_start_row_with_products()
+
+    @property
+    def start_row(self) -> int:
+        return self.find_start_row_with_products()
+
+    @property
+    def end_row(self) -> int:
+        return self.find_end_row_with_products()
 
     def find_start_row_with_products(self) -> int:
         """method for find start product table index
@@ -44,9 +65,9 @@ class ExselParser:
                 if cell.value is not None:
                     cell_text = str(cell.value).strip()
                     if cell_text == "Итого:":
-                        return r_idx - 1
+                        return r_idx
 
-        return self.sheet.max_row - 1
+        return self.sheet.max_row
 
 
 class ProductParser(ExselParser):
@@ -57,7 +78,9 @@ class ProductParser(ExselParser):
         self._delete_empty_columns()
         self.add_category_column()
         #####
-        self.additional_table_columns = self.delete_dublicate_title()
+        self.additional_table_columns = (
+            self.delete_dublicate_title()
+        )  # Массив индексов колонок внутри таблицы
 
     def delete_dublicate_title(self) -> List[int]:
         table_labels_title = self.df[self.df["№"] == "№"].index
@@ -68,8 +91,8 @@ class ProductParser(ExselParser):
     def add_category_column(self) -> None:
         target = "Артикул"
         idx = self.df.columns.get_loc(target)
-        print(idx)
         self.df.insert(loc=idx + 1, column="Категория", value=np.nan)
+        self.df.insert(loc=idx + 2, column="Подкатегория", value=np.nan)
 
     def _delete_empty_columns(self) -> None:
         cols_for_drop = []
@@ -82,8 +105,33 @@ class ProductParser(ExselParser):
         if cols_for_drop:
             self.df.drop(columns=cols_for_drop, inplace=True)
 
+    def add_category_to_product(self, product_name: str):
+        product_name = str(product_name).lower()
+
+        for category, subcategories in category_rule.items():
+            for subcategory, keywords in subcategories.items():
+                for keyword in keywords:
+                    # Простое вхождение подстроки:
+                    # TODO улучшить алгоритм
+                    if re.search(
+                        r"\b" + re.escape(keyword.lower()) + r"\b", product_name
+                    ):
+                        return category, subcategory
+        return "Не определена", "Не определена"
+
 
 parser = ProductParser(file_path)
 
 
+result = parser.df
+result[["Категория", "Подкатегория"]] = result["Товары (работы, услуги)"].apply(
+    lambda name: pd.Series(parser.add_category_to_product(name))
+)
+result.to_excel("data/result.xlsx", index=False)
+
+result.to_csv("data/result.csv", index=False, sep=";")
+
+
 print(parser.df.head(30))
+category_not_assign = len(result[result["Категория"] == "Не определена"])
+print("Категорий не определено: ", category_not_assign)
