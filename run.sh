1ac9f4848c7047fdee31e90d126f9ca5297a2ae8
WinerTy <nikitokmay@gmail.com>
2025-06-11 12:56:07 +0300

    Upd: build

--- run.sh
+++ run.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
-if ! command -v python3 &>/dev/null; then
-    echo "Python 3 не установлен. Установите Python 3.13 или новее."
-    exit 1
-fi
 
 
 if ! command -v uv &>/dev/null; then
     curl -LsSf https://astral.sh/uv/install.sh | sh
 fi
 
-uv venv --python python3.13
+
+if [! -d ".venv"]; then
+    uv venv --python python3.13
+fi
 
 source .venv/bin/activate
 
-uv pip install -r requirements.txt
+if [ -f "requirements.txt"];then
+    uv pip install -r requirements.txt
+else
+    uv sync
+fi
 
-uv run python src/parcer/main.py
+cd src/parcer/
+
+uv run python main.py
